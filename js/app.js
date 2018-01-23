40da99f184de953f13e557a95d9418f02d5b6e47
NatalyCortez <flyingchair_pe12@hotmail.com>
2018-01-23 14:45:16 -0500

    subiendo estructura

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var config = {
+    apiKey: "AIzaSyCT2WudtHE4bZ9V4vnXiyEiH_IydTKNJl8",
+    authDomain: "practica-86525.firebaseapp.com",
+    databaseURL: "https://practica-86525.firebaseio.com",
+    projectId: "practica-86525",
+    storageBucket: "",
+    messagingSenderId: "49347552884"
+  };
+  firebase.initializeApp(config);
+});
